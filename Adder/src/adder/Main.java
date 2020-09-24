9f88c860b746f4b576c31d1b49a3841cedb7b62c
NNTafos <71797286+NNTafos@users.noreply.github.com>
2020-09-23 23:29:51 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,20 @@
 package adder;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
         try {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide atleast two argument");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int sum = 0;
+        for(int i=0;i<args.length;i++)
+            sum += Integer.valueOf(args[i]);
+        return sum;
     }
 }
