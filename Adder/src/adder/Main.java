93e0f76ccfe65057b43848bf408c9123fa5bb681
HoldenEssman <31777020+HoldenEssman@users.noreply.github.com>
2020-09-24 23:38:49 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -15,19 +15,9 @@ public class Main {
     }
 
     private static int addArguments(String[] args) {
-
         int sum = 0;
         
-        if(args[0].equals("-")) { // if first argument is the subtraction sign (-), then subtract the remaining arguments
-            for(int i = 1; i < args.length; i++){
-                sum -= Integer.parseInt(args[i]);
-            }
-        }
-        else{
-            for(int i = 0; i < args.length; i++){
-                sum += Integer.parseInt(args[i]);
-            }
-        }
-        return sum;
+        for(int i = 0; i < args.length; i++){
+            sum += Integer.parseInt(args[i]);
     }
 }
