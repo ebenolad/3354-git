ff8d0f017c46b3ba4e52c5c09f7953146eabb429
HoldenEssman <31777020+HoldenEssman@users.noreply.github.com>
2020-09-24 23:06:41 -0500

    Update Main.java

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -6,27 +6,25 @@ public class Main {
         try {
             int result = addArguments(args);
             System.out.println(result);
-        } 
-        catch(NumberFormatException nfe){
+        } catch (NumberFormatException nfe) {
             System.err.println("Please provide valid input");
-        
-        catch (Exception e) {
+        } catch (Exception e) {
             System.err.println("Please provide two integers to add");
         }
     }
 
     private static int addArguments(String[] args) {
         int sum = 0;
-        
-        if(args[0].equals("-"){ // if first argument is the subtraction sign, then subtract the remaining arguments
-            for(int i = 1; i < args.length; i++){
+
+        if (args[0].equals("-")) { // if first argument is the subtraction sign, then subtract the remaining arguments
+            for (int i = 1; i < args.length; i++) {
                 sum -= Integer.parseInt(args[i]);
             }
-        }
-        else{
-            for(int i = 0; i < args.length; i++){ // add each argument to the sum
-            sum += Integer.parseInt(args[i]);
+        } else {
+            for (int i = 0; i < args.length; i++) { // add each argument to the sum
+                sum += Integer.parseInt(args[i]);
             }
+        }
         return sum;
     }
 }
